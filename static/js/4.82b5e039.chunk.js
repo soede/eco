94c69b22385c8c4eebe9012e3f23f2e72994c00f
unknown <adamnagoy@gmail.com>
2022-07-23 21:06:52 +0300

    Updates

---
+++ static/js/4.82b5e039.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmini-app"]=this["webpackJsonpmini-app"]||[]).push([[4],{206:function(i,p,a){"use strict";a.r(p);var n=a(203),s=a(204),t=a(205);n.init(),n.add(s),n.add(t),p.default=n}}]);
+//# sourceMappingURL=4.82b5e039.chunk.js.map
